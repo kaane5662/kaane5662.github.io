18bfc09572a8fd6ebb5ce89eacfea66f1a879e6f
kaane5662 <73482895+kaane5662@users.noreply.github.com>
2020-10-26 10:35:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Kaan Eren"
+let userName = "kaane5662";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
